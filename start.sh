5d623d7ad14046d36fefbc5b347f12b34e69c3e0
Johnny J <michaelcoffey5@gmail.com>
2026-03-05 16:57:24 -0500

    print out ip address

--- start.sh
+++ start.sh
@@ -10,5 +10,10 @@ docker exec meal_prep_dev bash -c "cd /home/devuser/meal_prep && make"
 # Run the server in the background inside the container
 docker exec -d meal_prep_dev ./build/meal_prep -s
 
-echo "Server is running at http://localhost:8080"
-echo "(It is also running on your Local IP address to access via phone)"
+# Fetch the current LAN IP address and print it clearly
+LAN_IP=$(ip -4 addr show scope global | grep -v 'docker\|br-\|veth' | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n 1)
+
+echo "------------------------------------------------------"
+echo "✅ Server is running at http://localhost:8080"
+echo "📱 Access from your phone at: http://${LAN_IP}:8080"
+echo "------------------------------------------------------"
